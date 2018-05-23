893b44cab85c843f9aeff5f188659ba11993f97e
lentrix <gijhaycosmod123@gmail.com>
2018-05-23 14:40:51 +0800

    Updated

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=ws_dalangin',
+    'username' => 'ws_dalangin',
+    'password' => 'Jeremias',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
